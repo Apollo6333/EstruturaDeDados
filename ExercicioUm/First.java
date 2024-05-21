6b9d0c7fcfc444fa127a58d58e5e01f63380bfe6
Apollo6333 <andreluiznunes1712@gmail.com>
2024-05-21 10:40:55 -0300

    a

---
+++ ExercicioUm/First.java
@@ -0,0 +1,24 @@
+package ExercicioUm;
+
+public class First {
+    private int[] lista;
+    private int tamanho;
+
+    public void listaEstatica() {
+        lista = new int[10];
+        tamanho = 0;
+    }
+
+    private void redimensionar() {
+        int[] listaRepo = new int[lista.length + 10];
+        for (int i = 0; i < lista.length; i++) {
+            listaRepo[i] = lista[i];
+        }
+        lista = listaRepo;
+        System.out.println("10 espaços adicionados a sua lista.");
+    }
+
+    public void inserir() {
+        if
+    }
+}
